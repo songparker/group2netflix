4ccf535ebb9179fb87bfb027d5d8030cf2c59236
chester <119461285+raeDV@users.noreply.github.com>
2023-01-27 12:09:43 -0500

    Update MainUser.java

--- MovieStreamingSystem/User/src/MainUser.java
+++ MovieStreamingSystem/User/src/MainUser.java
@@ -13,8 +13,8 @@ public class MainUser {
 	public static void main(String[] args) throws FileNotFoundException {
 		MainUser.User();
 	}
-		
-		public static void User() throws FileNotFoundException {
+
+	public static void User() throws FileNotFoundException {
 		Scanner enter = new Scanner(System.in);
 		int choice = 0;
 
@@ -26,7 +26,7 @@ public class MainUser {
 			// prompt users to choose one option
 			if (choice == 1) {
 				LogIn.Login();
-				MainUser.MainMenu();
+				MainUser.AdminMainMenu();
 
 			} else if (choice == 2) {
 				Register.Register();
@@ -46,24 +46,56 @@ public class MainUser {
 		Scanner enter = new Scanner(System.in);
 
 		System.out.println("Please choose the option you want to do from the shown list:");
-		System.out.println("1- \n" + "2- \n" + "3- \n" + "4- Search User\n"
-				+ "5- Watch History\n" + "6- Exit the System");
+		System.out.println(
+				"1- \n" + "2- \n" + "3- \n" + "4- Search User\n" + "5- Watch History\n" + "6- Exit the System");
 		int option = enter.nextInt();
 		switch (option) {
 		case 1:
-			
+
 			break;
 		case 2:
-			
+
 			break;
 		case 3:
-			
+
 			break;
 		case 4:
 			SearchingUser.SearchingUser();
 			break;
 		case 5:
 			WatchHistoryCheck.watchHistoryMenu();
+			break;
+		case 6:
+			System.exit(0);
+			break;
+		default:
+			System.out.println("Input error, please input again.");
+			break;
+		}
+	}
+
+	public static void AdminMainMenu() throws FileNotFoundException {
+		Scanner enter = new Scanner(System.in);
+
+		System.out.println("Please choose the option you want to do from the shown list:");
+		System.out.println("1- Adding Movie\n" + "2- Delete Movie\n" + "3- Edit Movie\n" + "4- Search Movie\n"
+				+ "5- Search User\n" + "6- Exit the System");
+		int option = enter.nextInt();
+		switch (option) {
+		case 1:
+			AddingMovie.AddingMovie();
+			break;
+		case 2:
+			DeleteMovie.deleteMovie();
+			break;
+		case 3:
+			EditMovie.editMovie();
+			break;
+		case 4:
+			SearchingMovie.SearchingMovie();
+			break;
+		case 5:
+			SearchingUser.SearchingUser();
 			break;
 		case 6:
 			System.exit(0);
